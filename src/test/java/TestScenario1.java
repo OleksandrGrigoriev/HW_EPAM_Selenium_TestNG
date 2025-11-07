c87188eef085fad42312f98977a417565aa8361b
Alexander Grigoriev <alexander.grigoriev.121987@gmail.com>
2025-11-07 12:01:02 +0200

    initial commit

---
+++ src/test/java/TestScenario1.java
@@ -0,0 +1,50 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestScenario1 {
+
+    private WebDriver driver;
+    protected BasePage basePage;
+    private final String AUT_URL = "https://www.amazon.com/";
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @BeforeMethod
+    public void loadApplication() {
+        driver.get(AUT_URL);
+        basePage = new BasePage();
+        basePage.setDriver(driver);
+    }
+
+    @Test
+    public void findSignInButtonOnBasePageTest() {
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".a-button-text span")));
+        WebElement signInButton = basePage.find(By.cssSelector(".a-button-text span"));
+        String textOfButton = signInButton.getText();
+
+        Assert.assertEquals(textOfButton, "Sign in");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
